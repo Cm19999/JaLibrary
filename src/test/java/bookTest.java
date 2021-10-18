75711599fba4d73d9da7a1b2dbfe1180a1452aee
callum <cmarsden4163@googlemail.com>
2021-10-18 16:42:31 +0100

    more tests

--- src/test/java/bookTest.java
+++ src/test/java/bookTest.java
@@ -16,4 +16,19 @@ public class bookTest {
         Paperback b= new Paperback("paper","Lenny","1");
         assertEquals("incorrect output","You have borrowed a paperback",b.borrowItem());
     }
+
+    @Test
+    public void testgetISBN() {
+        Paperback b= new Paperback("paper","Lenny","1");
+        assertEquals("incorrect output","1",b.getISBN());
+    }
+
+    @Test
+    public void testsetISBN() {
+        Paperback b= new Paperback("paper","Lenny","1");
+        b.setISBN("2");
+        assertEquals("incorrect output","2",b.getISBN());
+    }
+
+
 }
